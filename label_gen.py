f9cb6761777b6fe3299042fb26ac14ce29f8bce0
Tej-Deep <ptejdeep@gmail.com>
2023-03-16 12:35:53 +0800

    Added more examples in html

--- label_gen.py
+++ label_gen.py
@@ -1,13 +1,15 @@
 import os
 import cv2
 import numpy as np
+locations  = ['bedroom','bookstore', 'classroom', 'computer_lab']
+ids = ['00055', '00083', '00283', '00332']
+for loc, i in zip(locations,ids):
+    gt_path = f"./MIM-Depth-Estimation/data/nyu_depth_v2/official_splits/test/{loc}/sync_depth_{i}.png"
+    depth = cv2.imread(gt_path, cv2.IMREAD_UNCHANGED).astype('float32')
 
-gt_path = "./MIM-Depth-Estimation/data/nyu_depth_v2/official_splits/test/bathroom/sync_depth_00045.png"
-depth = cv2.imread(gt_path, cv2.IMREAD_UNCHANGED).astype('float32')
-
-save_path = "./MIM-Depth-Estimation/data/nyu_depth_v2/official_splits/label/bathroom_label_00045.png"
-# pred_d_numpy = pred_d.squeeze().cpu().numpy()
-depth = (depth / depth.max()) * 255
-depth = depth.astype(np.uint8)
-pred_d_color = cv2.applyColorMap(depth, cv2.COLORMAP_RAINBOW)
-cv2.imwrite(save_path, pred_d_color)
+    save_path = f"./MIM-Depth-Estimation/data/nyu_depth_v2/official_splits/label/{loc}_label_{i}.png"
+    # pred_d_numpy = pred_d.squeeze().cpu().numpy()
+    depth = (depth / depth.max()) * 255
+    depth = depth.astype(np.uint8)
+    pred_d_color = cv2.applyColorMap(depth, cv2.COLORMAP_RAINBOW)
+    cv2.imwrite(save_path, pred_d_color)
